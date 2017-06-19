3acbe6df1f91738172567c66f405d9ef5a618068
jaysinco <jaysinco@163.com>
2017-06-19 23:05:03 +0800

    code refacctoring phase 1

--- tenant.go
+++ tenant.go
@@ -2,214 +2,207 @@ package main
 
 import (
 	"bytes"
-	"flag"
+	"encoding/json"
 	"fmt"
-	"html/template"
-	"net/smtp"
-	"os"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"regexp"
+	"strconv"
 	"strings"
 	"time"
+
+	"github.com/yhat/scrape"
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
 )
 
-var groupFlag = flag.String("group", "shanghaizufang", "group name")
-var maxFlag = flag.Int("n", 25, "max num of posts")
-var keyFlag = flag.String("key",
-	"真北路|大渡河|金沙江路|娄山关路|威宁路|北新泾|淞虹路|中山公园|延安西路|虹桥路|"+
-		"曹杨路|上海体育馆|桂林路|漕河泾开发区|合川路|伊犁路|宋园路|水城路|龙溪路|"+
-		"宜山路|上海动物园|龙柏新村|紫藤路|虹桥1号航站楼",
-	"search key pattern")
-
-var fromFlag = flag.String("from", "zxk156@qq.com", "email sender")
-var toFlag = flag.String("to", "jaysinco@163.com;chenghuiasong@126.com",
-	"email receivers(';' separated)")
-var pwdFlag = flag.String("p", "", "email password")
-
-var keyRegxp *regexp.Regexp
-var postsPicked = make([]*Post, 0, 10)
-var dupCheck = make(map[string]bool)
-
 func main() {
-	start := time.Now()
-	flag.Parse()
-	fmt.Fprintln(os.Stderr, "filtering posts...")
-	keyRegxp = regexp.MustCompile(*keyFlag)
-	if err := (&Group{*groupFlag, *maxFlag}).EachPost(filter); err != nil {
-		fmt.Fprintf(os.Stderr, "main: filter post: %v\n", err)
+	// load config
+	cfg, err := loadConfig("config.json")
+	if err != nil {
+		log.Printf("[ERROR] load configure from 'config.json': %v", err)
+		return
 	}
-	fmt.Fprintln(os.Stderr, "fetching detail...")
-	done := make(chan error)
-	limit := make(chan struct{}, 10)
-	suceed := 0
-	for _, pt := range postsPicked {
-		post := pt
-		go func() {
-			limit <- struct{}{}
-			done <- post.GetDetail()
-			<-limit
-		}()
+
+	// search discuss following config
+	dcs, err := search(cfg.GroupID, cfg.MaxPage, cfg.MaxWorker, cfg.SearchKey)
+	if err != nil {
+		log.Printf("[ERROR] search: %v", err)
 	}
-	for i := 0; i < len(postsPicked); i++ {
-		if err := <-done; err == nil {
-			suceed += 1
+	log.Printf("[INFO ] %d discusses found", len(dcs))
+}
+
+// corresponding to json configure file
+type Config struct {
+	GroupID   string
+	MaxPage   int
+	MaxWorker int
+	SearchKey string
+}
+
+// load configural parameter from json file
+func loadConfig(filePath string) (*Config, error) {
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("read file: %v", err)
+	}
+	cfg := new(Config)
+	if err := json.Unmarshal(data, cfg); err != nil {
+		return nil, fmt.Errorf("unmarshal json: %v", err)
+	}
+	return cfg, nil
+}
+
+// discussion in douban group
+type Discuss struct {
+	Title     string
+	Link      string
+	ID        string
+	Author    string
+	Reply     int
+	LastReply time.Time
+}
+
+// search takes group name & max search page number & max search worker & search key
+// to filter discusses in douban group concurrently.
+func search(group string, pglimit int, mxworker int, srkey string) ([]*Discuss, error) {
+	// construct filter function
+	key, err := regexp.Compile(srkey)
+	if err != nil {
+		return nil, fmt.Errorf("compile search key '%s' as regexp: %v", srkey, err)
+	}
+	filter := func(d *Discuss) bool {
+		return key.Match([]byte(d.Title))
+	}
+
+	// setup worker goroutine and channel
+	type Outcome struct {
+		Out   []*Discuss
+		Wrong error
+	}
+	ocqueue := make(chan Outcome)
+	worker := func(lowlimit int, toplimit int) {
+		var wrong error
+		out := make([]*Discuss, 0)
+		for n := lowlimit; n <= toplimit; n++ {
+			url := fmt.Sprintf("https://www.douban.com/group/%s/discussion?start=%d",
+				group, n*25+1)
+			if dcs, err := collectDiscuss(url, filter); err != nil {
+				wrong = err
+				break
+			} else {
+				out = append(out, dcs...)
+			}
+		}
+		ocqueue <- Outcome{Out: out, Wrong: wrong}
+	}
+
+	// create workers
+	step := pglimit/mxworker + 1
+	for n := 0; n < mxworker; n++ {
+		lowlimit := n * step
+		toplimit := lowlimit + step - 1
+		if toplimit > pglimit {
+			toplimit = pglimit
+		}
+		go worker(lowlimit, toplimit)
+	}
+
+	// wait for workers to send back result
+	dcs := make([]*Discuss, 0)
+	erm := make(map[string]int)
+	for n := 0; n < mxworker; n++ {
+		outcome := <-ocqueue
+		dcs = append(dcs, outcome.Out...)
+		if outcome.Wrong != nil {
+			erm[outcome.Wrong.Error()]++
 		}
 	}
-	fmt.Fprintln(os.Stderr, "generating report...")
-	var buf bytes.Buffer
-	if err := template.Must(template.New("Post Report").
-		Parse(postReport)).Execute(&buf, Report{
-		Group:   *groupFlag,
-		Key:     *keyFlag,
-		Max:     *maxFlag,
-		Suceed:  suceed,
-		Created: time.Now(),
-		Posts:   postsPicked,
-	}); err != nil {
-		fmt.Fprintf(os.Stderr, "main: render report: %v\n", err)
-		os.Exit(1)
+
+	// return & report error
+	if len(erm) == 0 {
+		return dcs, nil
 	}
-	fmt.Fprintln(os.Stderr, "sending report...")
-	if *pwdFlag == "" {
-		fmt.Fprintln(os.Stdout, buf.String())
-	} else {
-		date := time.Now().Format("2006-01-02")
-		reportMail := SMTPMail{
-			From:     *fromFlag,
-			To:       *toFlag,
-			Password: *pwdFlag,
-			Subject:  fmt.Sprintf("%s %s", date, "豆瓣小组报告"),
-			Body:     buf.String(),
+	ers := new(bytes.Buffer)
+	ern := 0
+	for s, n := range erm {
+		ern += n
+		ers.WriteString(fmt.Sprintf("%s[%d times];", s, n))
+	}
+	return dcs, fmt.Errorf("%d/%d workers error interrupt: %v",
+		ern, mxworker, ers.String())
+}
+
+// discussGet returns douban discussions parsed from input url which has format like
+// 'https://www.douban.com/group/#group_id/discussion?start=#n' and filtered by input func.
+func collectDiscuss(url string, filter func(*Discuss) bool) ([]*Discuss, error) {
+	// communication check
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("unable connect to network")
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("http status code: %s", resp.Status)
+	}
+	root, err := html.Parse(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	if titleNode, ok := scrape.Find(root, scrape.ByTag(atom.Title)); ok &&
+		scrape.Text(titleNode) == "豆瓣" {
+		return nil, fmt.Errorf("response html title is '豆瓣'")
+	}
+
+	// filter out discuss links
+	matches := scrape.FindAll(root, func(n *html.Node) bool {
+		if n.DataAtom == atom.A && n.Parent != nil && n.Parent.DataAtom == atom.Td &&
+			scrape.Attr(n.Parent, "class") == "title" {
+			return true
 		}
-		if err := reportMail.Send(); err != nil {
-			fmt.Fprintf(os.Stderr, "main: send email: %v\n", err)
+		return false
+	})
+	if len(matches) == 0 {
+		return nil, fmt.Errorf("blank page matches no discuss link")
+	}
+
+	// get discuss basic info
+	var dcs = make([]*Discuss, 0, 25)
+	for _, linkNode := range matches {
+		var d Discuss
+		d.Link = scrape.Attr(linkNode, "href")
+		link := strings.TrimRight(d.Link, "/")
+		d.ID = link[strings.LastIndex(link, "/")+1:]
+		d.Title = scrape.Attr(linkNode, "title")
+		authorNode := linkNode.Parent.NextSibling.NextSibling
+		d.Author = scrape.Text(authorNode)
+		replyNode := authorNode.NextSibling.NextSibling
+		replyStr := scrape.Text(replyNode)
+		if replyStr == "" {
+			replyStr = "0"
+		}
+		if reply, err := strconv.Atoi(replyStr); err != nil {
+			return nil, fmt.Errorf("convert reply-num for '%s': %v", d.Title, err)
+		} else {
+			d.Reply = reply
+		}
+		lastReplyNode := replyNode.NextSibling.NextSibling
+		lastReplyStr := scrape.Text(lastReplyNode)
+		if len(lastReplyStr) == 10 {
+			lastReplyStr = fmt.Sprintf("%s 00:00 CST", lastReplyStr)
+		} else {
+			lastReplyStr = fmt.Sprintf("%d-%s CST", time.Now().Year(), lastReplyStr)
+		}
+		if lastReply, err := time.Parse("2006-01-02 15:04 MST",
+			lastReplyStr); err != nil {
+			return nil, fmt.Errorf("convert last-reply-time for '%s': %v", d.Title, err)
+		} else {
+			d.LastReply = lastReply
+		}
+		if filter == nil || filter(&d) {
+			dcs = append(dcs, &d)
 		}
 	}
-	fmt.Fprintf(os.Stderr, "program completed(%s)!\n", time.Since(start))
+	return dcs, nil
 }
-
-func filter(p *Post) error {
-	if keyRegxp.Match([]byte(p.Title)) && !dupCheck[p.Title] {
-		postsPicked = append(postsPicked, p)
-		dupCheck[p.Title] = true
-	}
-	return nil
-}
-
-type SMTPMail struct {
-	From     string
-	To       string
-	Password string
-	Subject  string
-	Body     string
-}
-
-func (e *SMTPMail) Send() error {
-	domain := e.From[strings.Index(e.From, "@")+1:]
-	auth := smtp.PlainAuth("", e.From, e.Password,
-		fmt.Sprintf("smtp.%s", domain))
-	msg := fmt.Sprintf("From: %s\r\n"+
-		"To: %s\r\n"+
-		"Content-Type: text/html; charset=UTF-8\r\n"+
-		"Subject: %s\r\n"+
-		"\r\n%s\r\n", e.From, e.To, e.Subject, e.Body)
-	if err := smtp.SendMail(fmt.Sprintf("smtp.%s:25", domain), auth,
-		e.From, strings.Split(e.To, ";"), []byte(msg)); err != nil {
-		return err
-	}
-	return nil
-}
-
-type Report struct {
-	Group   string
-	Max     int
-	Suceed  int
-	Key     string
-	Created time.Time
-	Posts   []*Post
-}
-
-const postReport = `
-<style type="text/css"> 
-	a:link {
-		text-decoration: none;
-		color: #37a;
-		background: transparent;
-	} 
-	a:visited {
-		text-decoration: none;
-		color: #666699;
-		background : transparent;
-	}
-	a:hover {
-	    color: #FFFFFF;
-	    text-decoration: none;
-	    background: #37a;
-	}
-	div.basic{ 
-		width:100%; 
-		background:#fff4e8; 
-		font-size:13px;
-		word-wrap:break-word;    
-		word-break:break-all;
-	}
-	table.gridtable {
-		width:100%;
-		font-size:13px;
-		color:#333333;
-		border-width: 1px;
-		border-color: #666666;
-		border-collapse: collapse;
-	}
-	table.gridtable th {
-		border-width: 1px;
-		padding: 8px;
-		border-style: solid;
-		border-color: #666666;
-		background-color: #dedede;
-	}
-	table.gridtable td {
-		border-width: 1px;
-		padding: 8px;
-		border-style: solid;
-		border-color: #666666;
-		background-color: #ffffff;
-	}
-</style> 
-<h1>
-	豆瓣小组报告
-</h1>
-<div class="basic">
-	<br>
-	<b>小组代码: &nbsp;</b>{{.Group}} 
-	<br>
-	<b>搜索数量: &nbsp;</b>{{.Max}}
-	<br>
-	<b>筛选数量: &nbsp;</b>{{.Posts | len}}
-	<br>
-	<b>详细数量: &nbsp;</b>{{.Suceed}}
-	<br>
-	<b>生成时间: &nbsp;</b>{{.Created.Format "2006-01-02 15:04:05"}}
-	<br>
-	<b>关键词: &nbsp;</b>
-	<br>
-	  <b>==>&nbsp;</b>{{.Key}}
-	<br>
-	<br>
-</div>
-<br>
-<table class="gridtable">
-	<tr>
-		<th><b>创建</b></th>	
-		<th><b>回应</b></th>
- 		<th width="70%"><b>话题</b></th>		
- 		<th><b>喜欢</b></th>
- 	</tr>
-	{{range .Posts}}
-	<tr>
-		<td>{{.Created.Format "2006-01-02"}}</a></td>
-		<td>{{.Reply}}</td>
-		<td><a href="{{.Link}}">{{.Title}}</a></td>
-		<td>{{.Favor}}</td>
-	</tr>
-	{{end}}
-</table>`
